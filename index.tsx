167620ff94ae56fb599e29a07534f4b9c5afdca3
pallavikasar8080 <pallavikasar8080@gmail.com>
2026-02-03 23:12:27 +0530

    Refactor index.tsx to simplify rendering logic
    Removed HashRouter and added error handling for root element.

--- index.tsx
+++ index.tsx
@@ -1,13 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
-import './components/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
